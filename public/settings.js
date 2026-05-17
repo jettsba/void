411e33424403fe71e54168cbdfc937e68653ed00
jettsba <krovmax@yandex.ru>
2026-05-17 12:10:04 +0400

    v0.5.11 - landing multiple fixes

--- public/settings.js
+++ public/settings.js
@@ -7,7 +7,7 @@
 (function () {
     "use strict";
 
-    const APP_VERSION = "0.5.2";
+    const APP_VERSION = "0.5.11";
     /* Экспортируем версию в window — log.bugReport() кладёт её в отчёт,
        чтобы было видно с какой версии собран дамп. */
     window.VoidVersion = APP_VERSION;
