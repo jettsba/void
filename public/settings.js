ddcd8c8cef7960663022b9ac0b632080f9a073a0
jettsba <krovmax@yandex.ru>
2026-05-19 23:10:20 +0400

    v0.8.3 - coturn: drop lt-cred-mech warning, disable TLS listeners, pin listening-ip/relay-ip to external IP

--- public/settings.js
+++ public/settings.js
@@ -7,7 +7,7 @@
 (function () {
     "use strict";
 
-    const APP_VERSION = "0.8.0";
+    const APP_VERSION = "0.8.3";
     /* Экспортируем версию в window — log.bugReport() кладёт её в отчёт,
        чтобы было видно с какой версии собран дамп. */
     window.VoidVersion = APP_VERSION;
