eb3e6787664497ddaeb5c8f81af683728a9de13a
jettsba <krovmax@yandex.ru>
2026-05-20 03:30:50 +0400

    v0.9.6 - landing: hide scrollbar & minor footer edits

--- public/settings.js
+++ public/settings.js
@@ -7,7 +7,7 @@
 (function () {
     "use strict";
 
-    const APP_VERSION = "0.9.2";
+    const APP_VERSION = "0.9.6";
     /* Экспортируем версию в window — log.bugReport() кладёт её в отчёт,
        чтобы было видно с какой версии собран дамп. */
     window.VoidVersion = APP_VERSION;
